6c05f75db2bf63db86dd17f330eb7d0ae6e15281
Evan Ricafort <evan.ricafort@gmail.com>
2021-07-24 00:16:34 +0800

    Update cooktest.php

--- p/cooktest.php
+++ p/cooktest.php
@@ -4,7 +4,7 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
 # opens a logfile where we save the query
-$fp = fopen('/p/log.txt','a');
+$fp = fopen('p/log.txt','a');
 
 fwrite($fp, $ip.' '.$browser."\n");
 fwrite($fp, urldecode($_SERVER['QUERY_STRING']). " \n\n");
