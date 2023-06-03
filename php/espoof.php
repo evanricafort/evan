efe7bad4b40474b06e7e5a74a74edf3a91d2dc71
Evan Ricafort <evan.ricafort@gmail.com>
2023-06-03 10:13:34 +0800

    Delete php directory

--- php/espoof.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "evanricafort@gmail.com";
-$subject = "Email Spoofing Test";
-$txt = "Email Spoofing Test by Evan.";
-$headers = "From: support@evanricafort.com";
-mail($to,$subject,$txt,$headers);
-?>
