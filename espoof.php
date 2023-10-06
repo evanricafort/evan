f1e210801cf55715c62c78c6a4c536693312c80a
Evan Ricafort <evan.ricafort@gmail.com>
2023-10-06 11:25:03 +0800

    Delete espoof.php

--- espoof.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "evanricafort@gmail.com";
-$subject = "Email Spoofing Test";
-$txt = "Test Email Spoofing.";
-$headers = "From: support@cellbrokerage.com";
-mail($to,$subject,$txt,$headers);
-?>
